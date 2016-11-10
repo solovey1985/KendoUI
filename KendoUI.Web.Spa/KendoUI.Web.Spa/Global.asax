eff94b87bc3bd92cf79a40c18828f23a26ffa341
Andrii Solovienko <osobuy@gmail.com>
2016-11-10 12:09:05 +0200

    [Web][SPA] - Solution for Single Page Application

---
+++ KendoUI.Web.Spa/KendoUI.Web.Spa/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KendoUI.Web.Spa.MvcApplication" Language="C#" %>
---
+++ KendoUI.Web.Spa/KendoUI.Web.Spa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KendoUI.Web.Spa.Startup))]
+namespace KendoUI.Web.Spa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
